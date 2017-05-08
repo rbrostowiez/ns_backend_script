452448d57741dc505fb1a68efe7286ac7ee0b706
Raymond Brostowicz <ray@screeninnovations.com>
2017-05-08 17:13:03 -0500

    Added new URLs and bumped the version

--- netsuites-tampermonkey.meta.js
+++ netsuites-tampermonkey.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name       Netsuite Backend Script
 // @namespace  raymond.brostowicz.com
-// @version    0.6
+// @version    0.7
 // @description  BackEnd scrips for NetSuite ERP
 // @match      https://system.na1.netsuite.com/*
 // @match      https://system.sandbox.netsuite.com/*
 // @copyright  2017 Raymond Brostowicz
-// @resource https://raw.githubusercontent.com/rbrostowiez/ns_backend_script/master/styles.css
+// @resource https://nsbs.brostowicz.com/styles.css
 // @require http://code.jquery.com/jquery-latest.js
 // @require http://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle.js
-// @require https://raw.githubusercontent.com/rbrostowiez/ns_backend_script/master/util.js
+// @require https://nsbs.brostowicz.com/util.js
 // @require https://system.na1.netsuite.com/javascript/NLAPI.jsp__NS_VER=2014.1.0&minver=145&locale=en_US.nlqs
-// @require https://raw.githubusercontent.com/rbrostowiez/ns_backend_script/master/netsuites-tampermonkey.js
+// @require https://nsbs.brostowicz.com/netsuites-tampermonkey.js
 // ==/UserScript==
