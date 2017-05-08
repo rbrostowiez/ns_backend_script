ea072aa2b38695208133897fb7ae1fe8ae88e9ea
Raymond Brostowicz <ray@screeninnovations.com>
2017-05-08 17:14:01 -0500

    removed ssl from urls to nsbs.brostowicz.com

--- netsuites-tampermonkey.meta.js
+++ netsuites-tampermonkey.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name       Netsuite Backend Script
 // @namespace  raymond.brostowicz.com
-// @version    0.7
+// @version    0.8
 // @description  BackEnd scrips for NetSuite ERP
 // @match      https://system.na1.netsuite.com/*
 // @match      https://system.sandbox.netsuite.com/*
 // @copyright  2017 Raymond Brostowicz
-// @resource https://nsbs.brostowicz.com/styles.css
+// @resource http://nsbs.brostowicz.com/styles.css
 // @require http://code.jquery.com/jquery-latest.js
 // @require http://twitter.github.io/typeahead.js/releases/latest/typeahead.bundle.js
-// @require https://nsbs.brostowicz.com/util.js
+// @require https//nsbs.brostowicz.com/util.js
 // @require https://system.na1.netsuite.com/javascript/NLAPI.jsp__NS_VER=2014.1.0&minver=145&locale=en_US.nlqs
-// @require https://nsbs.brostowicz.com/netsuites-tampermonkey.js
+// @require http://nsbs.brostowicz.com/netsuites-tampermonkey.js
 // ==/UserScript==
