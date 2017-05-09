f25e7b8fe926ed076337cdcbb7e16a64dc290287
Raymond Brostowicz <ray@screeninnovations.com>
2017-05-09 11:43:33 -0500

    Removed bad checks

--- netsuites-tampermonkey.meta.js
+++ netsuites-tampermonkey.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Netsuite Backend Script
 // @namespace  raymond.brostowicz.com
-// @version    0.46
+// @version    0.47
 // @description  BackEnd scrips for NetSuite ERP
 // @match      https://system.na1.netsuite.com/*
 // @match      https://system.sandbox.netsuite.com/*
