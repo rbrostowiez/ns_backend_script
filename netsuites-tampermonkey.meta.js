4654288588c755a060bc788d3ea5f836b478ba7f
Raymond Brostowicz <rbrostowiez@gmail.com>
2018-08-07 14:09:12 -0500

    Added version bump

--- netsuites-tampermonkey.meta.js
+++ netsuites-tampermonkey.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Netsuite Backend Script
 // @namespace  raymond.brostowicz.com
-// @version    0.1.5
+// @version    0.1.6
 // @description  BackEnd scrips for NetSuite ERP
 // @match      https://system.na1.netsuite.com/*
 // @match      https://system.sandbox.netsuite.com/*
